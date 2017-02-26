866151548c47f8e24e5bb2274738f8a237d7d908
Alejandro Figueroa <alejandro@ideasftw.com>
2017-02-26 18:23:58 -0500

    Add shebang

--- docker_update.rb
+++ docker_update.rb
@@ -1,5 +1,6 @@
+#!/usr/bin/env ruby
 # frozen_string_literal: true
 
 require 'docker'
 
-images = Docker::Image.all(filters: { dangling: ['false'] }.to_json).count
+images = Docker::Image.all filters: { dangling: ['false'] }.to_json
